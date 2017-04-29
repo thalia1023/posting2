c7cb65cefebea26a68aaefc9036f1fe63bbad5fb
Gahyun Kim <gahyunkk@likelion.org>
2017-04-29 16:43:22 +0000

    STEP 7: Add 'create' action to save user input to postings DB

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,4 +1,10 @@
 class PostingsController < ApplicationController
     def index
     end
+    def create
+    posting = Posting.new
+    posting.title = params[:title]
+    posting.posting_content = params[:content]
+    posting.save
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'postings#index'
+  get '/index' => 'postings#index'
+  post '/postings/create' => 'postings#create'
 end
