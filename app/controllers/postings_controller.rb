bb4b3f3802b076fc50bec39bedf85bf40e032dc4
Gahyun Kim <gahyunkk@likelion.org>
2017-05-08 09:37:42 +0000

    STEP12,13,14 CRUD model-Update & Delete

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -8,8 +8,37 @@ class PostingsController < ApplicationController
         posting = Posting.new
         posting.title = params[:title]
         posting.posting_content = params[:content]
-        posting.save
-    
-    redirect_to '/index'
+        if posting.save
+            redirect_to '/index'
+        else
+            flash[:error] = "Error : title cannot be less than 3 letters and content cannot be blank."
+            redirect_to '/index'
+        end
     end
+    
+    def destroy
+        posting = Posting.find(params[:id_of_posting])
+        posting.destroy
+        redirect_to '/index'
+    end
+    
+    def edit
+        @posting = Posting.find(params[:id_of_posting])
+    end
+    
+    def update
+        posting = Posting.find(params[:id_of_posting])
+        posting.title = params[:title]
+        posting.posting_content = params[:content]
+        if posting.save
+            redirect_to '/index'
+        else
+            flash[:error] = "Update Error : title cannot be less than 3 letters and content cannot be blank."
+            redirect_to '/index'
+        end
+    end        
 end
+
+
+
+
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,3 +1,5 @@
 class Posting < ActiveRecord::Base
     has_many :comments
+    validates :title, presence: true, length: { minimum: 3 }
+    validates :posting_content, presence: true
 end
